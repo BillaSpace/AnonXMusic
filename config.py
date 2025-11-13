0e4071619d1ceb74a16b229057265c2bc95cd3ed
Prakhar Shukla <SmokenX1@gmail.com>
2025-11-13 18:14:31 +0530

    Update config.py
    Signed-off-by: Prakhar Shukla <SmokenX1@gmail.com>

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ class Config:
     def __init__(self):
         self.API_ID = int(getenv("API_ID", 0))
         self.API_HASH = getenv("API_HASH")
-
+        self.API_URL = getenv("API_URL")
+        
         self.BOT_TOKEN = getenv("BOT_TOKEN")
         self.MONGO_URL = getenv("MONGO_URL")
 
@@ -22,18 +23,18 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-
-        self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
-        self.VIDEO_PLAY: bool = getenv("VIDEO_PLAY", True)
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BillaSpace")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BillaCore")
         self.COOKIES_URL = [
             url for url in getenv("COOKIES_URL", "").split(" ")
             if url and "batbin.me" in url
         ]
+        
+        self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
+        self.VIDEO_PLAY: bool = getenv("VIDEO_PLAY", True)
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
-        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
+        self.PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/0c8801bbeee21e47f2c0c-015b54f049cb726389.jpg")
+        self.START_IMG = getenv("START_IMG", "https://telegra.ph/file/b1dfce6ca97bff056a98b-7c37e3381ddc475660.jpg")
 
     def check(self):
         missing = [
